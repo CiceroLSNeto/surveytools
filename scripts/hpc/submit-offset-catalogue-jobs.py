ddcd2b0db194c48d0dbabb37ba54caba296dae0f
Geert Barentsen <geert@barentsen.be>
2015-05-14 19:57:59 +0100

    Added support for a BLACKLIST

--- scripts/hpc/submit-offset-catalogue-jobs.py
+++ scripts/hpc/submit-offset-catalogue-jobs.py
@@ -5,10 +5,14 @@ from astropy.table import Table
 
 from surveytools import SURVEYTOOLS_DATA
 
+# Blacklist based on the qc comments in Janet's image list
+BLACKLIST = ['0365b', '1929a', '1905a', '0636a', '0636b']
+
 images = Table.read(os.path.join(SURVEYTOOLS_DATA, 'vphas-dr2-red-images.fits'))
 fieldnumbers = [fld[6:] for fld in np.unique(images['Field_1'])]
 for field in fieldnumbers:
     for offsetname in ['a', 'b']:
         offset = field + offsetname
-        cmd = 'qsub -v OFFSET={} -N vphas_{} offset-catalogue.pbs'.format(offset, offset)
-        print(cmd)
+        if offset not in BLACKLIST:
+            cmd = 'qsub -v OFFSET={} -N vphas_{} offset-catalogue.pbs'.format(offset, offset)
+            print(cmd)
